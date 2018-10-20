9d1f4abbdeedc866acef45c9a0f8c0184be220f5
Pasheza <pashezik@gmail.com>
2018-10-20 18:36:19 +0300

    Saving image

--- mercari/mercariapp/views.py
+++ mercari/mercariapp/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from .forms import ItemForm, UploadPhotoForm
-from .handler import photo_handler
+import os
+
+from .forms import UploadPhotoForm
+
 
 
 def home(request):
@@ -10,12 +12,14 @@ def home(request):
 
 def new_item(request):
     if request.method == 'POST':
-        form = UploadPhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            print('Success')
-            return render(request, 'success_upload.html', {'filename': photo_handler(request.FILES['file'])})
-        print('Fuck you!')
-
+        file = request.FILES['file']
+        newpath = '~/toSave/'
+        if not os.path.exists(newpath):
+            os.makedirs(newpath, 0o777)
+        saveFile = open('~/toSave/' + file.name, 'w+')
+        saveFile.write(str(file.read()))
+        saveFile.close()
+        return HttpResponse("""{"status":"OK"}""", status=200)
     else:
         form = UploadPhotoForm()
     return render(request, 'sell-page/index.html', {'form': form})
